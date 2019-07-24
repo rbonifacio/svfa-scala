2b1eee4d9d7a20535bd56229038dd5ebb02d747b
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2019-07-24 20:04:50 +0200

    using PAG to deal with the load case

--- src/main/scala/br/unb/cic/soot/svfa/jimple/JSVFA.scala
+++ src/main/scala/br/unb/cic/soot/svfa/jimple/JSVFA.scala
@@ -9,9 +9,14 @@ import br.unb.cic.soot.graph.{Node, SinkNode}
 import br.unb.cic.soot.svfa.{SVFA, SourceSinkDef}
 import scalax.collection.GraphPredef._
 import soot.jimple._
+import soot.jimple.spark.pag
+import soot.jimple.spark.pag.{AllocNode, PAG}
+import soot.jimple.spark.sets.{DoublePointsToSet, HybridPointsToSet, P2SetVisitor, PointsToSetInternal}
 import soot.toolkits.graph.ExceptionalUnitGraph
 import soot.toolkits.scalar.SimpleLocalDefs
 import soot.{Local, PointsToSet, Scene, SceneTransformer, SootMethod, Transform}
+
+
 
 /**
   * A Jimple based implementation of
@@ -125,16 +130,17 @@ abstract class JSVFA extends SVFA with SourceSinkDef {
   }
 
   private def loadRule(stmt: AssignStmt, ref: InstanceFieldRef, method: SootMethod, defs: SimpleLocalDefs) : Unit = {
-    val queries = solver.findAllocationSites(method, stmt.base)
-    queries.foreach(q => {
-      val stmts = solver.findDefinitions(ref, q, pointsToAnalysis)
-      stmts.foreach(s => {
-        val source = createNode(s.getMethod, s.getUnit.get())
-        val target = createNode(method, stmt.base)
-        svg += source ~> target
-      })
-    })
+    val base = ref.getBase
+
+    if(base.isInstanceOf[Local] && pointsToAnalysis.isInstanceOf[PAG]) {
+      val pta = pointsToAnalysis.asInstanceOf[PAG]
+      val allocations = pta.reachingObjects(base.asInstanceOf[Local], ref.getField).asInstanceOf[DoublePointsToSet].getNewSet
+      allocations.asInstanceOf[HybridPointsToSet].forall(new AllocationVisitor(method, stmt.base))
+    }
   }
+
+
+
 
   private def defsToCallSite(caller: SootMethod, callee: SootMethod, calleeDefs: SimpleLocalDefs, callStmt: soot.Unit, retStmt: soot.Unit) = {
     val local = retStmt.asInstanceOf[ReturnStmt].getOp.asInstanceOf[Local]
@@ -175,4 +181,32 @@ abstract class JSVFA extends SVFA with SourceSinkDef {
   def isAssignReturnStmt(callSite: soot.Unit, unit: soot.Unit) : Boolean =
    unit.isInstanceOf[ReturnStmt] && unit.asInstanceOf[ReturnStmt].getOp.isInstanceOf[Local] &&
      callSite.isInstanceOf[soot.jimple.AssignStmt]
+
+  /*
+   * a class to visit the allocation nodes of the objects that
+   * a field might point to.
+   *
+   * @param method method of the statement stmt
+   * @param stmt statement with a load operation
+   */
+  class AllocationVisitor(val method: SootMethod, val stmt: soot.Unit) extends P2SetVisitor {
+    override def visit(n: pag.Node): Unit = {
+      if(n.isInstanceOf[AllocNode]) {
+        val allocationNode = n.asInstanceOf[AllocNode]
+        val body = allocationNode.getMethod.getActiveBody
+
+        body.getUnits.stream().forEach(unit => {
+          if(unit.isInstanceOf[soot.jimple.AssignStmt]) {
+            val exp = unit.asInstanceOf[soot.jimple.AssignStmt].getRightOp
+            if(exp.isInstanceOf[NewExpr] && allocationNode.getNewExpr == exp) {
+              val source = createNode(allocationNode.getMethod, unit)
+              val target = createNode(method, stmt)
+              svg + source ~> target
+            }
+          }
+          //TODO: should we also consider statments like return new ... or throw new ...?
+        })
+      }
+    }
+  }
 }
