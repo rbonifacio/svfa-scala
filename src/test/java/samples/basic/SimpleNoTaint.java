3cf5c6ba95f32958c80adad428976756e41b0705
jose clavo <jclavotafur@gmail.com>
2022-06-29 22:42:19 -0300

    add no taint sample

---
+++ src/test/java/samples/basic/SimpleNoTaint.java
@@ -0,0 +1,15 @@
+package samples.basic;
+
+public class SimpleNoTaint {
+
+    public void main(String args[]) {
+        int S1 = source();
+        int S2 = S1 + 3;
+        S2 = 5;
+        int S3 = S2 - 1;
+        sink(S3);
+    }
+
+    public int source() { return 1; }
+    public void sink(int data) { }
+}
