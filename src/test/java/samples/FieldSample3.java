cb6cd6799a628413a2f4189fd2584681a8d1d77e
jose clavo <jclavotafur@gmail.com>
2023-01-28 20:57:55 -0300

    Foo3 sample

--- src/test/java/samples/FieldSample3.java
+++ src/test/java/samples/FieldSample3.java
@@ -7,31 +7,41 @@ class Foo3 {
         this.blah = b;
     }
 
+//    int setBlah(int b) {
+//       return b;
+//    }
+
     int getBlah() {
         return this.blah;
     }
 }
 
-//public class FieldSample3 {
+public class FieldSample3 {
+
+    public void main() {
+        int a, b;
+        a = 999;
+        b = 111;
+
+
+        Foo3 A = new Foo3();
+        Foo3 B = new Foo3();
 //
-//    public void main() {
-//        Foo3 A = new Foo3();
-//        Foo3 B = new Foo3();
-////
-//        A.setBlah(source());   // SOURCE
-//        B.setBlah(999);       // SAFE
-////
-//        sink(A.blah); // SINK
-//        sink(B.blah); // OK
-//    }
+        A.setBlah(source());   // SOURCE
+//        A.setBlah(a);       // SAFE
+        B.setBlah(b);       // SAFE
 //
-//    int source() {
-//        return 0;
-//    }
-//
-//    void sink(int b) {
-//    }
-//}
+        sink(A.blah); // SINK
+        sink(B.blah); // OK
+    }
+
+    int source() {
+        return 0;
+    }
+
+    void sink(int b) {
+    }
+}
 
 //public class FieldSample3
 //{
@@ -39,8 +49,8 @@ class Foo3 {
 //        int a, b;
 //        int ap, bp;
 ////
-////        ap = source();
-//        ap = 1;
+//        ap = source();
+////        ap = 1;
 //        bp = 2;
 //
 //        a = myymethod(ap);
@@ -67,19 +77,24 @@ class Foo3 {
 
 //public class FieldSample3
 //{
-//    Integer x;
+//    int g;
 //
 //    public void main() {
-//        int a;
-//        a = source();
-////        myMethod(a);
-//        this.x = a;
-//        sink(a);
+//        int a, b;
+//        int x, y;
+//        a = 1;
+//        b = 2;
+//
+//        x = myMethod(a);
+//        y = myMethod(b);
+//
+//        sink(x);
+//        sink(y);
 //    }
 //
-//    void myMethod(int x)
+//    int myMethod(int x)
 //    {
-//        this.x = x;
+//        return x;
 //    }
 //
 //    int source() {
@@ -91,19 +106,25 @@ class Foo3 {
 //    }
 //}
 
-//class Z{}
+////class Z{}
 //class Z1 {
 //    int a, b;
 //}
-//class Z2 {}
+////class Z2 {}
 //
 //public class FieldSample3
 //{
 //    public void main() {
+//        int x, y;
+////        x = 999;
+//        y = 111;
 //        Z1 z1 = new Z1();
 //        Z1 z2 = new Z1();
+//
 //        z1.a = source();
-//        z2.a = 999;
+////        z1.a = x;
+//        z2.a = y;
+////        z2.a = source();
 //        sink(z1.a); //ERROR
 //        sink(z2.a); //OK
 //    }
@@ -120,39 +141,24 @@ class Foo3 {
 //}
 
 
-public class FieldSample3
-{
-    public void main() {
-        int a, b;
-
+//public class FieldSample3
+//{
+//    public void main() {
+//        int a, b, c;
+//
 //        a = source();
-//        b = 1;
 //        b = a;
-//        sink(b);
-    }
-
-    int source() {
-        return 0;
-    }
-
-    void sink(int b) {
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//        c = 1 + b;
+//        sink(c);
+//    }
+//
+//    int source() {
+//        return 0;
+//    }
+//
+//    void sink(int b) {
+//    }
+//}
 
 
 
