4864131a92668c2dc593253b69c87c66c5d9e20c
jose clavo tafur <jclavotafur@gmail.com>
2024-04-01 18:07:24 -0300

    add more tests

--- src/test/scala/br/unb/cic/soot/context/ContextTest.scala
+++ src/test/scala/br/unb/cic/soot/context/ContextTest.scala
@@ -46,7 +46,7 @@ class ContextTestSuite extends FunSuite {
   test("C2") {
     val svfa = new ContextTest("samples.context.Context2", "main")
     svfa.buildSparseValueFlowGraph()
-    print(svfa.svgToDotModel())
+//    print(svfa.svgToDotModel())
     assert(svfa.reportConflictsSVG().size == 1)
   }
 
@@ -57,10 +57,38 @@ class ContextTestSuite extends FunSuite {
     assert(svfa.reportConflictsSVG().size == 1)
   }
 
+  test("C3_PRE") {
+    val svfa = new ContextTest("samples.context.Context3Pre", "main")
+    svfa.buildSparseValueFlowGraph()
+    //    print(svfa.svgToDotModel())
+    assert(svfa.reportConflictsSVG().size == 1)
+  }
+
+  test("C3_POST") {
+    val svfa = new ContextTest("samples.context.Context3Post", "main")
+    svfa.buildSparseValueFlowGraph()
+    //    print(svfa.svgToDotModel())
+    assert(svfa.reportConflictsSVG().size == 1)
+  }
+
   test("C4") {
     val svfa = new ContextTest("samples.context.Context4", "main")
     svfa.buildSparseValueFlowGraph()
 //    print(svfa.svgToDotModel())
+    assert(svfa.reportConflictsSVG().size == 1)
+  }
+
+  test("C41") {
+    val svfa = new ContextTest("samples.context.Context41", "main")
+    svfa.buildSparseValueFlowGraph()
+    //    print(svfa.svgToDotModel())
+    assert(svfa.reportConflictsSVG().size == 1)
+  }
+
+  test("C42") {
+    val svfa = new ContextTest("samples.context.Context42", "main")
+    svfa.buildSparseValueFlowGraph()
+    //    print(svfa.svgToDotModel())
     assert(svfa.reportConflictsSVG().size == 1)
   }
 }
