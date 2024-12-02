c8912292c72865f3107bca030163642414a82b69
jose clavo tafur <jclavotafur@gmail.com>
2024-12-02 16:30:08 -0300

    enable test

--- src/test/scala/br/unb/cic/android/RoidsecTest.scala
+++ src/test/scala/br/unb/cic/android/RoidsecTest.scala
@@ -6,7 +6,7 @@ import soot.jimple.{AssignStmt, InvokeExpr, InvokeStmt}
 
 
 class RoidsecTest extends FunSuite {
-  ignore("in the class Basic2 we should detect 6 flows") {
+  test("in the class Basic2 we should detect 6 flows") {
     val svfa = new AndroidTaintBenchTest("roidsec")
     svfa.buildSparseValueFlowGraph()
     assert(svfa.reportConflictsSVG().size == 1)
