a44521a80e47bd4bea409a9701c51d558695dbe7
jose clavo tafur <jclavotafur@gmail.com>
2024-12-09 18:59:59 -0300

    svgToDotModel

--- src/test/scala/br/unb/cic/android/RoidsecTest.scala
+++ src/test/scala/br/unb/cic/android/RoidsecTest.scala
@@ -9,6 +9,7 @@ class RoidsecTest extends FunSuite {
   test("in the class Basic2 we should detect 6 flows") {
     val svfa = new AndroidTaintBenchTest("roidsec")
     svfa.buildSparseValueFlowGraph()
+    // println(svfa.svgToDotModel())
     assert(svfa.reportConflictsSVG().size == 1)
   }
 }
